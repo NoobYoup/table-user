9cba8120f81aef1d58d38470649333b1234a362c
NoobYoup <134846824+NoobYoup@users.noreply.github.com>
2025-03-05 10:36:01 +0700

    import excel

--- src/components/TableUsers.jsx
+++ src/components/TableUsers.jsx
@@ -1,14 +1,18 @@
 import { useEffect, useState } from 'react';
-import _ from 'lodash';
+import _, { debounce, get } from 'lodash';
+import { CSVLink, CSVDownload } from 'react-csv';
+import ReactPaginate from 'react-paginate';
+import Papa from 'papaparse';
 
 import Table from 'react-bootstrap/Table';
-import ReactPaginate from 'react-paginate';
 
 import { fetchAllUser } from '@/services/UserService';
 import ModalAddNew from '@/components/ModalAddNew';
 import ModalEditUser from '@/components/ModalEditUser';
 import ModalConfirm from '@/components/ModalConfirm';
+
 import '@/components/TableUser.scss';
+import { toast } from 'react-toastify';
 
 function TableUsers(props) {
     const [listUsers, setListUsers] = useState([]);
@@ -26,22 +30,8 @@ function TableUsers(props) {
     const [sortBy, setSortBy] = useState('asc');
     const [sortField, setSortField] = useState('id');
 
-    const handleClose = () => {
-        setIsShowModalAddNew(false);
-        setIsShowModalEditUser(false);
-        setIsShowModalDelete(false);
-    };
-
-    const handleUpdateTable = (user) => {
-        setListUsers([user, ...listUsers]);
-    };
-
-    const handleEditUserFromModal = (user) => {
-        let cloneListUsers = _.cloneDeep(listUsers);
-        let index = listUsers.findIndex((item) => item.id === user.id);
-        cloneListUsers[index].first_name = user.first_name;
-        setListUsers(cloneListUsers);
-    };
+    const [keyword, setKeyword] = useState('');
+    const [dataExport, setDataExport] = useState([]);
 
     useEffect(() => {
         // call API
@@ -58,27 +48,51 @@ function TableUsers(props) {
         }
     }
 
+    // xử lý đóng modal
+    const handleClose = () => {
+        setIsShowModalAddNew(false);
+        setIsShowModalEditUser(false);
+        setIsShowModalDelete(false);
+    };
+
+    // xử lý update table
+    const handleUpdateTable = (user) => {
+        setListUsers([user, ...listUsers]);
+    };
+
+    // xử lý sửa modal
+    const handleEditUserFromModal = (user) => {
+        let cloneListUsers = _.cloneDeep(listUsers);
+        let index = listUsers.findIndex((item) => item.id === user.id);
+        cloneListUsers[index].first_name = user.first_name;
+        setListUsers(cloneListUsers);
+    };
+
+    // xử lý phân trang
     const handlePageClick = (event) => {
         getUsers(event.selected + 1);
     };
 
+    // xử lý bật modal edit
     const handleEditUser = (user) => {
         setDataUserEdit(user);
         setIsShowModalEditUser(true);
     };
 
+    // xử lý bật modal delete
     const handleDeleteUser = (user) => {
         setIsShowModalDelete(true);
         setDataUserDelete(user);
-        console.log(user);
     };
 
+    // xử lý xóa user
     const handleDeleteUserFromModal = (user) => {
         let cloneListUsers = _.cloneDeep(listUsers);
         cloneListUsers = cloneListUsers.filter((item) => item.id !== user.id);
         setListUsers(cloneListUsers);
     };
 
+    // xử lý sort
     const handleSort = (sortBy, sortField) => {
         setSortBy(sortBy);
         setSortField(sortField);
@@ -88,13 +102,119 @@ function TableUsers(props) {
         setListUsers(cloneListUsers);
     };
 
+    // xử lý search
+    const handleSearch = debounce((term) => {
+        if (term) {
+            let cloneListUsers = _.cloneDeep(listUsers);
+            cloneListUsers = cloneListUsers.filter((item) => item.email.includes(term));
+            setListUsers(cloneListUsers);
+        } else {
+            getUsers(1);
+        }
+    }, 300);
+
+    const onChange = (event) => {
+        setKeyword(event.target.value);
+        handleSearch(event.target.value);
+    };
+
+    const getUsersExport = (event, done) => {
+        let result = [];
+        if (listUsers && listUsers.length > 0) {
+            result.push(['ID', 'Email', 'First name', 'Last name']);
+            listUsers.map((item, index) => {
+                let arr = [];
+                arr[0] = item.id;
+                arr[1] = item.email;
+                arr[2] = item.first_name;
+                arr[3] = item.last_name;
+                result.push(arr);
+            });
+            setDataExport(result);
+            done();
+        }
+    };
+
+    const handleImportCSV = (event) => {
+        if (event.target && event.target.files && event.target.files[0]) {
+            let file = event.target.files[0];
+
+            if (file.type !== 'text/csv') {
+                toast.error('Only accept CSV file...');
+                return;
+            }
+
+            Papa.parse(file, {
+                // header: true,
+                complete: function (results) {
+                    let rawCSV = results.data;
+                    if (rawCSV.length > 0) {
+                        if (rawCSV[0] && rawCSV[0].length === 3) {
+                            if (
+                                rawCSV[0][0] !== 'email' ||
+                                rawCSV[0][1] !== 'first_name' ||
+                                rawCSV[0][2] !== 'last_name'
+                            ) {
+                                toast.error('Wrong format Header...');
+                            } else {
+                                let result = [];
+
+                                rawCSV.map((item, index) => {
+                                    if (index > 0 && item.length === 3) {
+                                        let obj = {};
+                                        obj.email = item[0];
+                                        obj.first_name = item[1];
+                                        obj.last_name = item[2];
+                                        result.push(obj);
+                                    }
+                                });
+                                setListUsers(result);
+                            }
+                        } else {
+                            toast.error('Wrong format CSV file...');
+                        }
+                    } else {
+                        toast.error('Not found data...');
+                    }
+                },
+            });
+        }
+    };
+
     return (
         <>
             <div className="app-container my-3 d-flex justify-content-between">
-                <p>List Users:</p>
-                <button className="btn btn-success" onClick={() => setIsShowModalAddNew(true)}>
-                    Add new user
-                </button>
+                <h5>List Users:</h5>
+                <div>
+                    <label htmlFor="import" className="btn btn-info">
+                        Import
+                    </label>
+                    <input type="file" id="import" hidden onChange={(event) => handleImportCSV(event)} />
+
+                    <CSVLink
+                        data={dataExport}
+                        filename={'my-file.csv'}
+                        className="btn btn-primary mx-2"
+                        target="_blank"
+                        asyncOnClick={true}
+                        onClick={getUsersExport}
+                    >
+                        Export
+                    </CSVLink>
+
+                    <button className="btn btn-success" onClick={() => setIsShowModalAddNew(true)}>
+                        Add new user
+                    </button>
+                </div>
+            </div>
+
+            <div className="my-3">
+                <input
+                    className="form-control"
+                    placeholder="Search user by email..."
+                    value={keyword}
+                    onChange={onChange}
+                />
             </div>
 
             <Table striped bordered hover>
